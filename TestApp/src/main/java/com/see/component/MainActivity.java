494a0bf3ae609bbd4678345ea4696378ed43adf8
XiaoSe <420961648@qq.com>
2017-10-20 18:12:45 +0800

    init

---
+++ TestApp/src/main/java/com/see/component/MainActivity.java
@@ -0,0 +1,16 @@
+package com.see.component;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author WuXiang
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
