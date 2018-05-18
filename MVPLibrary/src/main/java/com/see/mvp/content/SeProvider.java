d744ea48e4c40f75fd3b320e556edf9bc65e0b11
XiaoSe <420961648@qq.com>
2018-05-18 14:46:30 +0800

    添加对Android7.0以上FileProvider支持

---
+++ MVPLibrary/src/main/java/com/see/mvp/content/SeProvider.java
@@ -0,0 +1,50 @@
+package com.see.mvp.content;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+/**
+ * @author by WuXiang on 2018/5/18.
+ * 适配Android7.0以上的Uri权限问题
+ */
+public class SeProvider {
+
+    public static Uri getUriForFile(Context context, File file) {
+        Uri fileUri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            fileUri = getUriForFile24(context, file);
+        } else {
+            fileUri = Uri.fromFile(file);
+        }
+        return fileUri;
+    }
+
+    public static Uri getUriForFile24(Context context, File file) {
+        return FileProvider.getUriForFile(context, getAuthority(context), file);
+    }
+
+    public static String getAuthority(Context context) {
+        return context.getPackageName() + ".fileprovider";
+    }
+
+    public static void setIntentDataAndType(Context context,
+                                            Intent intent,
+                                            String type,
+                                            File file,
+                                            boolean writeAble) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setDataAndType(getUriForFile(context, file), type);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            if (writeAble) {
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+        } else {
+            intent.setDataAndType(Uri.fromFile(file), type);
+        }
+    }
+}
